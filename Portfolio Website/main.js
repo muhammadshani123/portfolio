30c672b52a15c48703d687ccacffa8b26310f254
muhammadshani123 <154463138+muhammadshani123@users.noreply.github.com>
2024-11-18 21:43:53 +0500

    Add files via upload

---
+++ Portfolio Website/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Frontend Developer", "React Developer", "Web Developer"],
+    typeSpeed : 50,
+    backSpeed : 70,
+    backDelay : 1000,
+    loop : true
+});
